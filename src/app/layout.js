71e15ab2b89af9d16e0c04b268aac49a28347ae0
Chamod Malindu <chamod10malindu@gmail.com>
2025-10-16 16:37:40 +0530

    Add Toaster component for notifications in RootLayout 🧩

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionProvider from "./providers/SessionProvider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,8 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>
-          {children}
+          <Toaster position="top-right" />
+            {children}
         </SessionProvider>
 
       </body>
