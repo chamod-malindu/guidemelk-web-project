22246ee8a83071973a9e5f566c1d4b9189bc612c
Chamod Malindu <chamod10malindu@gmail.com>
2026-03-20 09:24:21 +0530

    fix: add .js extension to eslint-config-next import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
